a8d6a44e4ff38eee48a550407b41ebcf3dd98abf
Kylon Tyner <tynerk92@gmail.com>
2021-12-01 17:52:46 -0500

    trying mailchimp server function

--- app/netlify/functions/mailchimp.js
+++ app/netlify/functions/mailchimp.js
@@ -1,6 +1,8 @@
+import mailchimp from '../../src/mailchimp.js'
 exports.handler = async function(event, context) {
+  const res = mailchimp.lists.getAllLists()
   return {
-        statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
-    };
+    statusCode: 200,
+    body: JSON.stringify(res)
+  }
 }
