38e3b9cb3d14aacbe330431e147ebd8fbe6d9d04
Kylon Tyner <tynerk92@gmail.com>
2021-12-01 17:29:17 -0500

    Update mailchimp.js

--- app/src/mailchimp.js
+++ app/src/mailchimp.js
@@ -1,8 +1,15 @@
 import mailchimp from '@mailchimp/mailchimp_marketing'
-console.log('setting mailchimp config');
+
 mailchimp.setConfig({
   apiKey: '123444c62655a5158dd90c6e967d78ac',
   server: 'us20'
 })
+
+async function run() {
+  const repsonse = await mailchimp.ping.get()
+  console.log(response)
+}
+
+run()
 
 export default mailchimp
