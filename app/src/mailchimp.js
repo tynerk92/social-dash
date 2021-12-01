5495f19654b78fecfc20f4ee60310f8e4a91784a
Kylon Tyner <tynerk92@gmail.com>
2021-12-01 17:27:01 -0500

    Update mailchimp.js

--- app/src/mailchimp.js
+++ app/src/mailchimp.js
@@ -1,7 +1,7 @@
 import mailchimp from '@mailchimp/mailchimp_marketing'
 console.log('setting mailchimp config');
 mailchimp.setConfig({
-  apiKey: '123444c62655a5158dd90c6e967d78ac-us20',
+  apiKey: '123444c62655a5158dd90c6e967d78ac',
   server: 'us20'
 })
 
