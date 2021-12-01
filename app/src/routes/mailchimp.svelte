91726edb4ca25f0e7ffbe26740438cb328d66c49
Kylon Tyner <tynerk92@gmail.com>
2021-12-01 16:58:11 -0500

    Display mailchimp lists

--- app/src/routes/mailchimp.svelte
+++ app/src/routes/mailchimp.svelte
@@ -1,20 +1,20 @@
 <script>
   import mailchimp from '../mailchimp.js'
   import { onMount } from 'svelte'
-  
+
   let lists = [];
 
-  onMount(() => {
+  onMount(async () => {
     console.log("Mounting mailchimp");
-    // const data = await mailchimp.lists.getAllLists()
-    // console.log( data)
-    // return data.lists
+    const data = await mailchimp.lists.getAllLists()
+    console.log( data)
+    return data.lists
   })
 
 </script>
 
 <ul>
-  <!-- {#each lists as list (list.id)}
-    <li>{list}</li>
-  {/each} -->
+  {#each lists as list (list.id)}
+    <li>{list.name}</li>
+  {/each}
 </ul>
