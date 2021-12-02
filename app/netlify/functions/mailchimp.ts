1f15efcd39f94124f302cfa7b9a7616e2c942bec
Kylon Tyner <tynerk92@gmail.com>
2021-12-01 20:35:39 -0500

    Update mailchimp.ts

--- app/netlify/functions/mailchimp.ts
+++ app/netlify/functions/mailchimp.ts
@@ -1,4 +1,4 @@
-const mailchimp = require('@mailchimp/mailchimp_marketing')
+import mailchimp from '@mailchimp/mailchimp_marketing'
 
 mailchimp.setConfig({
   apiKey: process.env.MAILCHIMP_API_KEY,
